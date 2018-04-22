91878d7c5e866e26af3879ea63bc2a6158a595d7
Makarenko <demo6112@gmail.com>
2018-04-23 01:54:23 +0300

    Hash pass by Rfc2898DeriveBytes

--- Lab4/Lab4.BLL/Services/UserService.cs
+++ Lab4/Lab4.BLL/Services/UserService.cs
@@ -16,16 +16,57 @@ namespace Lab4.BLL.Services
             DbContext = dbContext;
         }
 
-        private string HashPassword(string password)
+        public string HashPassword(string password)
         {
-            MD5 md5 = System.Security.Cryptography.MD5.Create();
-            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(password);
-            byte[] hash = md5.ComputeHash(inputBytes);
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++) {
-                sb.Append(hash[i].ToString("X2"));
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null) {
+                throw new ArgumentNullException("password");
             }
-            return sb.ToString();
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8)) {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
+        }
+
+        public bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] buffer4;
+            if (hashedPassword == null) {
+                return false;
+            }
+            if (password == null) {
+                throw new ArgumentNullException("password");
+            }
+            byte[] src = Convert.FromBase64String(hashedPassword);
+            if ((src.Length != 0x31) || (src[0] != 0)) {
+                return false;
+            }
+            byte[] dst = new byte[0x10];
+            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
+            byte[] buffer3 = new byte[0x20];
+            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8)) {
+                buffer4 = bytes.GetBytes(0x20);
+            }
+            return ByteArraysEqual(buffer3, buffer4);
+        }
+
+        private bool ByteArraysEqual(byte[] buffer3, byte[] buffer4)
+        {
+            if (buffer3.Length == 0)
+                return false;
+            if (buffer3.Length != buffer4.Length)
+                return false;
+            for (int i = 0; i < buffer3.Length; i++) {
+                if (buffer3[i] != buffer4[i])
+                    return false;
+            }
+            return true;
         }
 
         public User SignUp(User user)
@@ -55,9 +96,14 @@ namespace Lab4.BLL.Services
         public User SignIn(string login, string password)
         {
             var hashedPassword = HashPassword(password);
-            return DbContext.Users
-                .Where(u => u.Login == login && u.Password == hashedPassword)
+            var user = DbContext.Users
+                .Where(u => u.Login == login)
                 .FirstOrDefault();
+            if (user == null)
+                return null;
+            if (VerifyHashedPassword(user.Password, password))
+                return user;
+            return null;
         }
     }
 }
