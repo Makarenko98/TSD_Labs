1cad5fb3ffa78ac0d43ccd4649cc3ed3fb9cb9b4
Makarenko <demo6112@gmail.com>
2018-05-01 17:26:51 +0300

    HomeController

--- Lab4/Lab4.WebApp/Controllers/HomeController.cs
+++ Lab4/Lab4.WebApp/Controllers/HomeController.cs
@@ -6,15 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Lab4.WebApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Lab4.BLL;
+using Lab4.BLL.Models;
+using Dapper;
+using Lab4.BLL.Constants;
 
 namespace Lab4.WebApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        [Authorize]
+        private SocialNetDbContext dbContext;
+        public HomeController(SocialNetDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var user = dbContext.Users
+                .Include(u => u.UserFriends)
+                .ThenInclude((UserFriend uf) => uf.Friend)
+                .Include(u => u.FriendRequests)
+                .ThenInclude((FriendRequest fr) => fr.FromUser)
+                .Where(u => u.Login == HttpContext.User.Identity.Name)
+                .FirstOrDefault();
+
+            user.FriendRequests = user.FriendRequests.Where(fr => fr.StateId == FriendRequestStateConstants.New).ToArray();
+
+            return View(user);
         }
 
         public IActionResult Error()
