567648adc804cc9f7614834e961fe6ab7d5366be
Makarenko <demo6112@gmail.com>
2018-05-01 17:31:51 +0300

    PhothoController

--- Lab4/Lab4.WebApp/Controllers/PhotoController.cs
+++ Lab4/Lab4.WebApp/Controllers/PhotoController.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Lab4.BLL.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Lab4.WebApp.Controllers
 {
     [Produces("application/json")]
     [Route("api/Photo")]
+    [Authorize]
     public class PhotoController : Controller
     {
         public FileResult GetUserProfilePhoto(int userId)
