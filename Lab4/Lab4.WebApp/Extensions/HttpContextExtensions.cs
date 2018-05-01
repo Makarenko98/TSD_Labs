8758c35594991443a9159fc5a9c0e8df74699786
Makarenko <demo6112@gmail.com>
2018-05-01 17:22:44 +0300

    HttpContextExtension GetUser

---
+++ Lab4/Lab4.WebApp/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Lab4.BLL;
+using Lab4.BLL.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.WebApp
+{
+    public static class HttpContextExtensions
+    {
+        public static User GetUser(this HttpContext httpContext, SocialNetDbContext dbContext)
+        {
+            return dbContext.Users.Where(u => u.Login == httpContext.User.Identity.Name).FirstOrDefault();
+        }
+    }
+}
