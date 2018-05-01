497a22c0c2b7db932c3d6131c9807f4275213299
Makarenko <demo6112@gmail.com>
2018-05-01 17:24:28 +0300

    FriendController

---
+++ Lab4/Lab4.WebApp/Controllers/FriendController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab4.BLL.Services;
+using Lab4.BLL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.WebApp.Controllers
+{
+    public class FriendController : Controller
+    {
+        private FriendService friendService;
+        private SocialNetDbContext dbContext;
+
+        public FriendController(SocialNetDbContext dbContext, FriendService friendService)
+        {
+            this.friendService = friendService;
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public void SendFriendRequest(int userId)
+        {
+            friendService.SendFriendRequest(
+                HttpContext.GetUser(dbContext).Id,
+                userId);
+        }
+
+        [HttpPost]
+        public bool AcceptFriendRequest(int friendRequestId)
+        {
+            friendService.AcceptFriendRequest(dbContext.FriendRequests.Find(friendRequestId));
+            return true;
+        }
+
+        [HttpPost]
+        public bool RejectFriendRequest(int friendRequestId)
+        {
+            friendService.RejectFriendRequest(dbContext.FriendRequests.Find(friendRequestId));
+            return true;
+        }
+    }
+}
